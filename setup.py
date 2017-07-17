ba23206d70ffe67324e4f31da381418462482f83
Fabian Köhler <fkoehler1024@googlemail.com>
2017-07-17 21:53:30 +0200

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="dotgen",
+    version="0.1",
+    description="Dotfiles generator",
+    author="Fabian Köhler",
+    author_email="fkoehler1024@googlemail.com",
+    scripts=["./dotgen"],
+    url="https://github.com/f-koehler/dotgen")
